0a2627593472b93cca851633495b8001d9432ffa
Daniel Winning <daniel@winningwebdesign.co.uk>
2024-02-10 03:01:29 +0000

    allow only select elements for now

--- src/JSelect.ts
+++ src/JSelect.ts
@@ -1,20 +1,11 @@
 class JSelect {
-    private element: HTMLInputElement|HTMLSelectElement;
-    private readonly HTML_INPUT_CLASS: string = 'HTMLInputElement';
+    private element: HTMLSelectElement;
     private readonly HTML_SELECT_CLASS: string = 'HTMLSelectElement';
-    private readonly allowedHTMLElements: Array<string> = [
-        this.HTML_INPUT_CLASS,
-        this.HTML_SELECT_CLASS
-    ]
 
     constructor(element: HTMLElement) {
-        const elementClass: string = element.constructor.name;
+        if (element.constructor.name !== this.HTML_SELECT_CLASS) return;
 
-        if (!this.allowedHTMLElements.includes(elementClass)) return;
-
-        this.element = elementClass === this.HTML_INPUT_CLASS
-            ? <HTMLInputElement> element
-            : <HTMLSelectElement> element;
+        this.element = <HTMLSelectElement> element;
     }
 }
 
