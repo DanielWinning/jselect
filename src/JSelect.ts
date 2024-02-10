6f14dcedefd014be91875e58d2b86fdb9335fe61
Daniel Winning <daniel@winningwebdesign.co.uk>
2024-02-10 02:57:14 +0000

    initialize JSelect on input/select field

--- src/JSelect.ts
+++ src/JSelect.ts
@@ -1,6 +1,20 @@
 class JSelect {
-    constructor(name: string) {
-        console.log(`Hello, ${name}`);
+    private element: HTMLInputElement|HTMLSelectElement;
+    private readonly HTML_INPUT_CLASS: string = 'HTMLInputElement';
+    private readonly HTML_SELECT_CLASS: string = 'HTMLSelectElement';
+    private readonly allowedHTMLElements: Array<string> = [
+        this.HTML_INPUT_CLASS,
+        this.HTML_SELECT_CLASS
+    ]
+
+    constructor(element: HTMLElement) {
+        const elementClass: string = element.constructor.name;
+
+        if (!this.allowedHTMLElements.includes(elementClass)) return;
+
+        this.element = elementClass === this.HTML_INPUT_CLASS
+            ? <HTMLInputElement> element
+            : <HTMLSelectElement> element;
     }
 }
 
