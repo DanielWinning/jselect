8cfc2aec50b84dc7ba64d52a0db21f5dbeaa453e
Daniel Winning <daniel@winningwebdesign.co.uk>
2024-02-11 03:36:29 +0000

    removed builder export, currently only intended for use internally

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,7 @@
 declare interface IJSelectOptions {
 
 }
-declare class JSelectBuilder {
-    public static instantiateAllSelectsOnPage(): void;
-}
 declare class JSelect {
     constructor(element: HTMLElement, options: IJSelectOptions = {});
     public static loadAllWithDefaultOptions(): void;
 }
-export { JSelect, JSelectBuilder };
